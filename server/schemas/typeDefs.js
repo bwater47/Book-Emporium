d86b02f9f2723613ce6883f1a9256db03d9aab6e
B Water <brennw47@gmail.com>
2024-07-25 13:06:03 -0500

    feat(resolvers): add functionality for queries and mutations  Added the functionality for the queries and mutations in the resolvers. Added to the server/utils/auth.js the authentication error object which I included by importing it from graphql.
    feat(resolvers): add functionality for queries and mutations
    
    Added the functionality for the queries and mutations in the resolvers. Added to the server/utils/auth.js the authentication error object which I included by importing it from graphql.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,36 +1,43 @@
 const typeDefs = gql`
-type Query {
+  type Query {
     me: [User]
-}
+  }
 
-type Mutation {
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+    saveBook(
+      authors: [String]
+      description: String
+      title: String
+      bookId: String
+      image: String
+      link: String
+    ): User
     removeBook(bookId: String): User
-}
+  }
 
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     bookCount: Int
     savedBooks: [Book]
-}
+  }
 
-type Book {
+  type Book {
     bookId: String
     authors: [String]
     description: String
     title: String
     image: String
     link: String
-}
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User
-}
+  }
 `;
 
 module.exports = typeDefs;
