e98260cc565789f04cf787fbdbde9aceb8d6f633
B Water <brennw47@gmail.com>
2024-07-26 15:38:48 -0500

    feat(LoginForm): integrate Apollo mutations  Added the import for mutations from the Apollo Client package and imported the login_user mutation. Implemented the loginuser state for managing mutation functionality and refactored the try-catch block to handle data and await mutations.  Ensures proper integration of login functionality with Apollo Client.
    feat(LoginForm): integrate Apollo mutations
    
    Added the import for mutations from the Apollo Client package and imported the login_user mutation. Implemented the loginuser state for managing mutation functionality and refactored the try-catch block to handle data and await mutations.
    
    Ensures proper integration of login functionality with Apollo Client.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -3,13 +3,15 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,14 +29,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
+      // Console.log(data)
+      const token = data.login.token;
       Auth.login(token);
     } catch (err) {
       console.error(err);
